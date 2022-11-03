c8be7a6f05ef24d0ccf2ffb7122b4af8e8fe7e4f
RuiZ <stbenji@MacBook-Pro.fritz.box>
2022-11-03 14:33:03 +0100

    fix symlink

---
+++ shellcheck.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+shellcheck -e SC1071 ./script/*.sh ./tools/**/*.sh ./system/**/*.sh
--- tools/oc/install.sh
+++ tools/oc/install.sh
@@ -6,13 +6,13 @@ fi
 
 if test ! $(command -v oc); then
   echo "Installing OpenShift CLI"
-  mkdir -p "${DOTFILE_HOME}/openshift-cli/tmp"
+  mkdir -p "${DOTFILE}/oc/tmp"
   curl -sSL "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-${OS_NAME}.tar.gz" > "${DOTFILE_HOME}/openshift-cli/tmp/oc.tar.gz"
   
-  tar xvzf "${DOTFILE_HOME}/openshift-cli/tmp/oc.tar.gz" --directory "${DOTFILE_HOME}/openshift-cli/tmp" "oc" 
+  tar xvzf "${DOTFILE}/oc/tmp/oc.tar.gz" --directory "${DOTFILE_HOME}/openshift-cli/tmp" "oc" 
   
-  cp "${DOTFILE_HOME}/openshift-cli/tmp/oc" "$HOME/bin/oc"
+  cp "${DOTFILE}/oc/tmp/oc" "$HOME/bin/oc"
   
   sudo chmod +x "$HOME/bin/oc"
-  rm -rf "${DOTFILE_HOME}/openshift-cli/tmp"
+  rm -rf "${DOTFILE}/oc/tmp"
 fi
