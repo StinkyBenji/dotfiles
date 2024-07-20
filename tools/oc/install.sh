dcd0e471a43292da4a6d0e47b5552fdc59890848
StinkyBenji <ether852@gmail.com>
2024-07-20 22:57:36 +0200

    fix: remove deprecated brew tap

--- tools/oc/install.sh
+++ tools/oc/install.sh
@@ -11,7 +11,7 @@ if test ! "$(command -v oc)"; then
   echo "Installing OpenShift CLI"
   
   mkdir -p "${DOTFILES}/tools/oc/tmp"
-  curl -sSL "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.9.25/openshift-client-${OS_NAME}.tar.gz" > "${DOTFILES}/tools/oc/tmp/oc.tar.gz"
+  curl -sSL "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.14.9/openshift-client-${OS_NAME}.tar.gz" > "${DOTFILES}/tools/oc/tmp/oc.tar.gz"
   
   tar xvzf "${DOTFILES}/tools/oc/tmp/oc.tar.gz" --directory "${DOTFILES}/tools/oc/tmp" "oc" 
   
