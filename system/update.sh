6a8c48f379dbb71240d2b59425ea3539449e1800
Rui Zhang <ruizhang@Ruis-MBP.fritz.box>
2024-11-22 12:55:16 +0100

    update

--- system/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if test "$(uname)" = "Linux"; then
-    sh -c "sudo dnf upgrade -y" 
-else
-    sh -c "sudo softwareupdate -i -a"
-fi  
-    
-  
--- tools/vscode/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# this installs some vscode-extensions
-
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension EditorConfig.EditorConfig
-code --install-extension esbenp.prettier-vscode
-code --install-extension k3a.theme-dark-plus-contrast
-
